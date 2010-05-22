2329ee6e97908f003b2efc7fb727271c1abdb92d
Mikko Keinänen <mikko.keinanen@gmail.com>
2010-05-22 12:06:39 +0300

    Added license information

--- src/db/databasemanager.h
+++ src/db/databasemanager.h
@@ -1,3 +1,22 @@
+// EmuFront
+// Copyright Mikko Keinänen 2010
+//
+// This file is part of EmuFront.
+//
+//
+// EmuFront is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Foobar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef DATABASEMANAGER_H
 #define DATABASEMANAGER_H
 
@@ -14,22 +33,21 @@ public:
 	DatabaseManager(QObject *parent = 0);
 	~DatabaseManager();
 
+    virtual QSqlTableModel* getDataModel() = 0;
     static bool openDB();
     static bool deleteDB();
     static bool dbExists();
     static QSqlError lastError();
     static bool createDB();
-    virtual QSqlTableModel* getDataModel() = 0;
     void resetModel();
 
 protected:
     QSqlTableModel *sqlTableModel;
 
 private:
-    //static QSqlDatabase db;
 	static const QString DB_FILENAME;
-    static QString getDbPath();
     virtual QSqlTableModel* getData() = 0;
+    static QString getDbPath();
 };
 
 #endif
