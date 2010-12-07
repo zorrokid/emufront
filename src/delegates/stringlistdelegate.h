c8b567e37966d5282fdff6649effceecaa6d4524
Mikko Keinänen <mikko.keinanen@gmail.com>
2010-12-07 22:42:50 +0200

    Started implementing StringListDelegate.

---
+++ src/delegates/stringlistdelegate.h
@@ -0,0 +1,37 @@
+// EmuFront
+// Copyright 2010 Mikko Keinänen
+//
+// This file is part of EmuFront.
+//
+//
+// EmuFront is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation and appearing in the file gpl.txt included in the
+// packaging of this file.
+//
+// EmuFront is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EmuFront.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef STRINGLISTDELEGATE_H
+#define STRINGLISTDELEGATE_H
+
+#include <QStyledItemDelegate>
+
+class StringListDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit StringListDelegate(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // STRINGLISTDELEGATE_H
