2c513693bb5b5248bb8e6e04dc2e1873a55028d2
Mikko Keinänen <mikko.keinanen@gmail.com>
2010-12-12 02:07:44 +0200

    Added new model: ExternalExecutableModel.

---
+++ src/models/externalexecutablemodel.cpp
@@ -0,0 +1,66 @@
+/*
+** EmuFront
+** Copyright 2010 Mikko Keinänen
+**
+** This file is part of EmuFront.
+**
+**
+** EmuFront is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 2 as published by
+** the Free Software Foundation and appearing in the file gpl.txt included in the
+** packaging of this file.
+**
+** EmuFront is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with EmuFront.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "externalexecutablemodel.h"
+
+ExternalExecutableModel::ExternalExecutableModel(QObject *parent) :
+    EmuFrontQueryModel(parent)
+{
+    refresh();
+}
+
+void ExternalExecutableModel::refresh()
+{
+    setQuery(constructSelect());
+    setHeaderData(Executable_Id, Qt::Horizontal, tr("Id"));
+    setHeaderData(Executable_Name, Qt::Horizontal, tr("Name"));
+    setHeaderData(Executable_Executable, Qt::Horizontal, tr("Executable"));
+    setHeaderData(Executable_Options, Qt::Horizontal, tr("Options"));
+    setHeaderData(Executable_TypeId, Qt::Horizontal, tr("Type"));
+    setHeaderData(Executable_SetupId, Qt::Horizontal, tr("Setup id"));
+    setHeaderData(Executable_SetupName, Qt::Horizontal, tr("Setup"));
+}
+
+QString ExternalExecutableModel::constructSelect(QString where) const
+{
+    return QString("SELECT "
+                   "executable.id AS ExecutableId, "
+                   "executable.name AS ExecutableName, "
+                   "executable.executable AS Executable, "
+                   "executable.options AS ExecutableOptions, "
+                   "executable.type AS ExecutableType, "
+                   "setup.id As ExecutableSetupId, "
+                   "platform.name || ' ' || mediatype.name AS SetupName "
+                   "FROM executable "
+                   "INNER JOIN setup ON executable.setupid = setup.id "
+                   "INNER JOIN platform ON setup.platformid=platform.id "
+                   "INNER JOIN mediatype ON setup.mediatypeid=mediatype.id "
+                   "%1 "
+                   "ORDER BY executable.name").arg(where);
+}
+
+Qt::ItemFlags ExternalExecutableModel::flags(const QModelIndex &index) const
+{
+    Qt::ItemFlags flags = QSqlQueryModel::flags(index);
+    int col = index.column();
+    // TODO
+    return flags;
+}
