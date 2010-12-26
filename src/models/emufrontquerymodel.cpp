6f8d8943b02591d152b9e8c61af81e3b7db243e7
Mikko Keinänen <mikko.keinanen@gmail.com>
2010-12-27 01:22:46 +0200

    Just testing new EmuFront repository at Gitorious.

--- src/models/emufrontquerymodel.cpp
+++ src/models/emufrontquerymodel.cpp
@@ -18,6 +18,7 @@
 ** You should have received a copy of the GNU General Public License
 ** along with EmuFront.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include "emufrontquerymodel.h"
 #include "emufrontobject.h"
 #include "platform.h"
